89edf1580f8832a819fa9bd8c1d5b310a0110177
Poire Molle <s224311@dtu.dk>
2023-04-06 20:33:24 +0200

    Removing unused import statements and function calls

--- main.c
+++ main.c
@@ -1,9 +1,6 @@
-#include <stdio.h>
-#include <string.h>
 #include "Deck.c"
 int main() {
-//saveDeck();
-//loadDeck();
+
     //createDeck("cards.txt");
     printDeck(shuffleInterweave(13,createDeck("cards.txt")));
 
