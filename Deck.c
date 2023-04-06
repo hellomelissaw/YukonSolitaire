e4f0a875e2ac6fa4c100f82da56ea09f220d5d5e
Poire Molle <s224311@dtu.dk>
2023-04-06 20:32:20 +0200

    Cleaning up unused code, adding ressource links in comments and fixing shuffle bug

--- Deck.c
+++ Deck.c
@@ -1,6 +1,9 @@
 //
 // Created by Mélissa on 2023-04-03.
 // ressources : https://www.w3schools.com/c/c_files_read.php
+//              https://www.programiz.com/c-programming/c-structures-pointers
+//              https://www.programiz.com/dsa/linked-list
+//
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -17,30 +20,6 @@ struct card
     struct card *next;
 
 };
-
-/*const char* CARD_LABELS[CARD_COUNT] = {
-        "AC", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "TC", "JC", "QC", "KC",
-        "AD", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "TD", "JD", "QD", "KD",
-        "AH", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "TH", "JH", "QH", "KH",
-        "AS", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "TS", "JS", "QS", "KS"
-};*/
-
-/*char** loadCardLabels(char fileName[]) {
-    char** cardLabels = NULL;
-    FILE* fpointer = fopen(fileName, "r"); // creates a pointer to the read file
-
-    char cardLabel[LABEL_SIZE];
-    for(int i = 0 ; i < CARD_COUNT ; i++) {
-        fgets(cardLabel, LABEL_SIZE, fpointer);
-        cardLabels[i] = malloc(LABEL_SIZE);
-        cardLabels[i] = cardLabel;
-    }
-    fclose(fpointer);
-
-    fprintf("%s", cardLabels[0]);
-
-   return cardLabels;
-}*/
 
 // FUNCTION TO CREATE AND RETURN A NEW CARD (NODE IN LINKED LIST)
 struct card* createCard(char rank, char suit) {
@@ -61,41 +40,17 @@ struct card* createDeck(char fileName[]) {
     }
     fclose(fpointer);
 
-
-   /* for (int j = 0 ; j < CARD_COUNT ; j++) {
-        printf("For card #%d, rank is %c and its suit is %c\n", j, (char)cardLabels[j][0], (char)cardLabels[j][1]);
-    }*/
-
    /* Add values from the cardLabels array to the array of cards */
-    //struct card* cards[CARD_COUNT]; // make a pointer to array of cards
     struct card* head = createCard(cardLabels[0][0], cardLabels[0][1]);
     struct card* current = head;
     for(int i = 1 ; i < CARD_COUNT ; i ++) // get the rank and suit from line i of the file and pass as argument to createCard func
     {
         char rank = cardLabels[i][0];
         char suit = cardLabels[i][1];
-        //cards[i] = createCard(rank, suit);
         current->next = createCard(rank, suit);
         current = current->next;
-       /* if(i > 0) {
-            cards[i-1]->next = cards[i];
-        }*/
 
     }
-
-    /* Add the first card's next card and make it head of Linked List
-    cards[0]->next = cards[1];
-    struct card *head = cards[0];
-     */
-
-   /* Test printing all cards in linked list
-    int k = 0;
-    current = head;
-    while(current != NULL) {
-        printf("Card #%d is %c%c\n", k, current->rank, current->suit);
-        current = current->next;
-        k++;
-    }*/
 
     return head;
 }
@@ -121,13 +76,6 @@ struct card* shuffleInterweave(int split, struct card* head) {
             currentHead = currentHead->next;
     }
 
-
-    struct card* current1 = tempHead1;
-    //printf("Card in pile 1 is %c%c\n", tempHead1->rank, tempHead1->suit);
-  /* while(current1 != NULL) {
-        printf("Card in pile 1 is %c%c\n", current1->rank, current1->suit);
-        current1 = current1->next;
-    }*/
 
 /*create first card in temp list2 then all the other cards until OG list head reaches NULL */
     struct card* tempHead2 = NULL;
@@ -156,44 +104,19 @@ struct card* shuffleInterweave(int split, struct card* head) {
     struct card* shuffledDeckHead = head;
     struct card* currentTempHead1 = tempHead1;
     struct card* currentTempHead2 = tempHead2;
-    /*printf("cth1 = %c%c, cth1->next = %c%c, cth1->next->next = %c%c, cth1->next->next->next = %c%c, cth1->next->next->next-> = %c%c\n",
-           currentTempHead1->rank,currentTempHead1->suit,
-           currentTempHead1->next->rank,currentTempHead1->next->suit,
-           currentTempHead1->next->next->rank,currentTempHead1->next->next->suit,
-           currentTempHead1->next->next->next->rank, currentTempHead1->next->next->next->suit,
-           currentTempHead1->next->next->next->next->rank, currentTempHead1->next->next->next->next->suit);
-
-    printf("cth2 = %c%c, cth2->next = %c%c, cth2->next->next = %c%c, cth2->next->next->next = %c , cth2->next->next->next-> = %c \n",
-           currentTempHead2->rank,currentTempHead2->suit,
-           currentTempHead2->next->rank,currentTempHead2->next->suit,
-           currentTempHead2->next->next->rank,currentTempHead2->next->next->suit,
-           currentTempHead2->next->next->next->rank,
-           currentTempHead2->next->next->next->next->suit
-           );*/
 
     shuffledDeckHead->rank = currentTempHead1->rank;
     shuffledDeckHead->suit = currentTempHead1->suit;
-    //shuffledDeckHead->next = head->next;
-    /*if(shuffledDeckHead->next == NULL) {
-        printf("IT'S NULL!");
-    } else {
-        shuffledDeckHead->next->rank = currentTempHead2->rank;
-        shuffledDeckHead->next->suit = currentTempHead2->suit;
-    }*/
-    //printf("card #0 is %c%c\n", shuffledDeckHead->rank, shuffledDeckHead->suit);
-    //printf("card #1 is %c%c\n", shuffledDeckHead->next->rank, shuffledDeckHead->next->suit);
 
     current = shuffledDeckHead;
-    //printf("current card after assigning it shuffledeck->next %c%c\n", current->rank, current->suit);
+
 
     for(int k = 2; k < CARD_COUNT ; k++) {
         current->next = currentTempHead2;
         currentTempHead2 = currentTempHead2->next;
-       // printf("currentTempHead2 after assigning it to the next node: %c%c\n", currentTempHead2->rank, currentTempHead2->suit);
 
         current->next->next = currentTempHead1;
         currentTempHead1 = currentTempHead1->next;
-        //printf("currentTempHead1 after assigning it to the next node: %c%c\n", currentTempHead1->rank, currentTempHead1->suit);
 
         if(currentTempHead1 == NULL || currentTempHead2 == NULL){
             break;
@@ -204,11 +127,18 @@ struct card* shuffleInterweave(int split, struct card* head) {
     }
 
     if(currentTempHead1 == NULL) {
-        current->next = currentTempHead2;
-        currentTempHead2 = currentTempHead2->next;
+        while(currentTempHead2 != NULL){
+            current->next = currentTempHead2;
+            current = current->next;
+            currentTempHead2 = currentTempHead2->next;
+        }
+
     } else if (currentTempHead2 == NULL) {
-        current->next = currentTempHead1;
-        currentTempHead1 = currentTempHead1->next;
+        while(currentTempHead1 != NULL) {
+            current->next = currentTempHead1;
+            current = current->next;
+            currentTempHead1 = currentTempHead1->next;
+        }
     }
 
    return shuffledDeckHead;
@@ -220,4 +150,5 @@ int printDeck(struct card* head) {
         printf("card #%d: %c%c\n", i, current->rank, current->suit);
         current = current->next;
     }
+    return 0;
 }
