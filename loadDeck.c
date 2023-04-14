62b5d3e9951ef5430f4a199880e8b58beab73151
sabirinomar <s211150@dtu.dk>
2023-04-14 10:37:54 +0200

    still on loading deck of cards

---
+++ loadDeck.c
@@ -0,0 +1,153 @@
+//
+// Created by Admin
+//
+
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define NUM_SUITS 4
+#define NUM_CARDS_PER_SUIT 13
+#define NUM_CARDS 52
+
+// Arrays to store suit and rank names
+char* suits[] = {"Clubs", "Diamonds", "Hearts", "Spades"};
+char* ranks[] = {"A", "2", "3", "4", "5", "6", "7",
+                 "8", "9", "10", "J", "Q", "K"};
+
+
+//struct linkedList
+struct linkedList{
+    struct node *head;
+};
+
+// Struct to represent a card
+typedef struct {
+    char rank[3];
+    char suit[8];
+    bool isVisible;
+    struct card *next;
+} Card;
+
+
+// Array to store the deck of cards
+Card deck[NUM_CARDS];
+
+// Function to check if a card is valid
+int is_valid_card(char* rank, char* suit) {
+    int valid_rank = 0, valid_suit = 0;
+
+    // Check if the rank is valid
+    for (int i = 0; i < NUM_CARDS_PER_SUIT; i++) {
+        if (strcmp(rank, ranks[i]) == 0) {
+            valid_rank = 1;
+            break;
+        }
+    }
+
+    // Check if the suit is valid
+    for (int i = 0; i < NUM_SUITS; i++) {
+        if (strcmp(suit, suits[i]) == 0) {
+            valid_suit = 1;
+            break;
+        }
+    }
+
+    return (valid_rank && valid_suit);
+}
+
+// Function to load a deck of cards from a file or create a new unshuffled deck if no filename is given
+int load_deck(char* filename) {
+    FILE *fp;
+    char buffer[20];
+    int card_count = 0, line_number = 0, error = 0;
+
+    if (filename == NULL) {
+        // Create a new unshuffled deck of cards
+        for (int i = 0; i < NUM_SUITS; i++) {
+            for (int j = 0; j < NUM_CARDS_PER_SUIT; j++) {
+                strcpy(deck[card_count].rank, ranks[j]);
+                strcpy(deck[card_count].suit, suits[i]);
+                card_count++;
+            }
+        }
+        printf("OK\n");
+        return 0;
+    }
+
+    // Open the file
+    fp = fopen(filename, "r");
+    if (fp == NULL) {
+        // File does not exist
+        printf("Error: File does not exist\n");
+        return -1;
+    }
+
+    // Read each line of the file
+    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+        line_number++;
+        buffer[strcspn(buffer, "\n")] = '\0'; // Remove newline character
+
+        // Parse the line into rank and suit
+        char *rank = strtok(buffer, " ");
+        char *suit = strtok(NULL, " ");
+
+        // Check if the card is valid
+        if (!is_valid_card(rank, suit)) {
+            printf("Error: Invalid card at line %d\n", line_number);
+            error = 1;
+            break;
+        }
+
+        // Add the card to the deck
+        strcpy(deck[card_count].rank, rank);
+        strcpy(deck[card_count].suit, suit);
+        card_count++;
+    }
+
+    fclose(fp);
+
+    // Check for errors
+    if (error) {
+        return -1;
+    }
+
+    if (card_count != NUM_CARDS) {
+        printf("Error: Incorrect number of cards in file\n");
+        return -1;
+    }
+
+}
+
+
+//
+
+
+
+
+//
+// Just a test for me.
+//
+
+
+//int main(int argc, char* argv[]) {
+    //If argc is 1, it means no filename was given as input.
+    // So, we call the load_deck function with NULL parameter to create a new unshuffled deck of cards */
+//if (argc == 1) {
+//load_deck(NULL);
+//}
+
+ //If argc is 2, it means a filename was given as input. So, we call the load_deck function with the filename provided as argument
+//else if (argc == 2) {
+//load_deck(argv[1]);
+//}
+
+ //If argc is greater than 2,it means too many arguments were provided. So, we print an error message and return -1
+//else {
+//printf("Error: Too many arguments\n");
+//return -1;
+//}
+ //return 0 to indicate successful completion of the program
+//return 0;
+//}
+
--- main.c
+++ main.c
@@ -1,7 +1,55 @@
 #include "Deck.c"
+#include <stdio.h>
+#include <string.h>
 int main() {
 
     //createDeck("cards.txt");
     printDeck(shuffleInterweave(13,createDeck("cards.txt")));
 
+
 }
+
+//MAKE LAYOUT FOR PLAYING THE GAME IN TERMINAL.
+
+
+
+
+
+/*
+//struct newNode newNode;
+int main() {
+    char input[2];
+    char load[] = "LD";
+
+
+    while (1) {
+
+        printf("\tC1   \tC2  \tC3  \tC4  \tC5  \tC6  \tC7");
+        printf("\n");
+        printf("                                                           []  \tF1\n");
+        printf("                                                           []  \tF2\n");
+        printf("                                                           []  \tF3\n");
+        printf("                                                           []  \tF4\n");
+
+        printf("");
+        printf("LAST command; \n");
+        printf("Message \n");
+        printf("INPUT >");
+
+        scanf("%s", &input);
+
+        //TJEK FOR OM ARG == 0,.
+
+        if (!strcmp(input, "QQ")) {
+            printf("quitting the program");
+            exit(0);
+        }
+        if (strcmp(input, "LD")){
+
+        }
+        if (strcmp(input, "Q") == 0) {
+            //Resetgame();
+        }
+    }
+}
+ */
