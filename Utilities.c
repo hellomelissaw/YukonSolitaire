fc68b31d0a6c61199e9a61a0bf7bf1f064d8c06d
Poire Molle <s224311@dtu.dk>
2023-04-17 13:56:55 +0200

    Adding a if statement to check if windows computer

--- Utilities.c
+++ Utilities.c
@@ -4,8 +4,37 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <limits.h>
+//#include <unistd.h>
+#ifdef _WIN32 // If on Windows
+#include <direct.h>
+    #define GetCurrentDir _getcwd
+#else // If on Mac or Linux
 #include <unistd.h>
+#define GetCurrentDir getcwd
+#endif
 
+char* getAbs_path(char fileName[]) {
+    char cwd[PATH_MAX];
+    getcwd(cwd, sizeof(cwd));
+
+    char* abs_path;
+#ifdef _WIN32
+    char full_path[_MAX_PATH];
+    if (_fullpath(full_path, fileName, _MAX_PATH) != NULL) {
+        abs_path = full_path;
+    } else {
+        // handle error case
+        abs_path = NULL;
+    }
+#else
+    chdir(".."); // go to parent dir to get absolute file path from project's parent directory
+    abs_path = realpath(fileName, NULL);
+    chdir(cwd);
+#endif
+
+    return abs_path;
+}
+/*
 /// FUNCTION TO GET THE ABSOLUTE PATH TO ANY GIVEN FILE
 /// \param fileName char array of the name of the file to find the path to
 /// \return pointer
@@ -18,4 +47,4 @@ char* getAbs_path(char fileName[]) {
     chdir(cwd);
 
     return abs_path;
-}
+}*/
