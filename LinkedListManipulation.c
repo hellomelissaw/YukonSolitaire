4d1fb8b31d03e09fba059844572f9e80da815e3c
Poire Molle <s224311@dtu.dk>
2023-04-24 22:01:55 +0200

    tidying up unused code

--- LinkedListManipulation.c
+++ LinkedListManipulation.c
@@ -160,7 +160,6 @@ bool validateMoveToColumn(Card* src, Pile** destColumn) {
 }
 
 void moveToColumn(Pile** src, Pile** destColumn, char cardToBeMovedRank){
-    //Pile* pileToBeMoved = (*src);
     Card* tempHead = (*src)->head;
     Card* newTail = NULL;
     while(tempHead->rank != cardToBeMovedRank) {
@@ -169,26 +168,16 @@ void moveToColumn(Pile** src, Pile** destColumn, char cardToBeMovedRank){
     }
 
 
-   /* while(pileToBeMoved->head->rank != cardToBeMovedRank) {
-        newTail = pileToBeMoved->head;
-        pileToBeMoved->head = pileToBeMoved->head->next;
-
-    }*/
     if(validateMoveToColumn(tempHead, destColumn)) {
         while(tempHead != NULL) {
             insertAtTail(&tempHead,&(*destColumn)->head, &(*destColumn)->tail);
         }
-   /* if(validateMoveToColumn(pileToBeMoved->head, destColumn)) {
-        while(pileToBeMoved->head != NULL) {
-            insertAtTail(&pileToBeMoved->head, &(*destColumn)->head, &(*destColumn)->tail);
-        }*/
 
         setNewTail(src, newTail);
 
     } else {
         printf("Could not move card %c%c.\n", tempHead->rank, tempHead->suit);
     }
-    //printf("current card is: %c%c", pileToBeMoved->head->rank, pileToBeMoved->head->suit);
 
 }
 
